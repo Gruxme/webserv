b3db45c2b6033325a23e75fb8479858c924cc8c7
sel-fadi <sel-fadi@student.1337.ma>
2022-02-04 18:12:13 +0100

    Still fix env for php-cgi

--- CGI/cgi.cpp
+++ CGI/cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 12:52:15 by sel-fadi          #+#    #+#             */
-/*   Updated: 2022/02/03 20:30:43 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2022/02/04 18:01:38 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,88 +19,89 @@
 
 
 const std::string ENV[4] = {
-    "QUERY_STRING", "REQUEST_METHOD", "SCRIPT_FILENAME", "SCRIPT_NAME"
+	"QUERY_STRING", "REQUEST_METHOD", "SCRIPT_FILENAME", "SCRIPT_NAME"
 };
 
-void setEnv()
+char *const*setEnv(std::vector<std::string> my_env)
 {
-    std::vector<std::string> my_env;
-	my_env.push_back("SERVER_PROTOCOL=HTTP/1.1");
-	my_env.push_back("SERVER_SOFTWARE=webserv");
-	my_env.push_back("GATEWAY_INTERFACE=CGI/1.1");
+	char **env = new char*[my_env.size()];
+	
+	for (int i; i < my_env.size(); i++)
+		env[i]= (char *)my_env[i].c_str();
+	env[my_env.size() + 1] = NULL;
+	return (env);
+}
+
+std::vector<std::string> setEnvInVector()
+{
+	std::vector<std::string> my_env;
 	my_env.push_back("CONTENT_LENGTH=");
 	my_env.push_back("CONTENT_TYPE=");
+	my_env.push_back("GATEWAY_INTERFACE=CGI/1.1");
 	my_env.push_back("PATH_INFO=");
+	my_env.push_back("PATH_TRANSLATED=");
 	my_env.push_back("QUERY_STRING=");
 	my_env.push_back("REQUEST_METHOD=");
 	my_env.push_back("REMOTE_ADDR=");
 	my_env.push_back("SCRIPT_NAME=");
 	my_env.push_back("SERVER_NAME=");
 	my_env.push_back("SERVER_PORT=");
-	my_env.push_back("PATH_TRANSLATED=");
+	my_env.push_back("SERVER_PROTOCOL=HTTP/1.1");
+	my_env.push_back("SERVER_SOFTWARE=webserv");
 	my_env.push_back("REDIRECT_STATUS=200");
-    
-    my_env[3].append("99");
-    std::cout << my_env[3] <<  std::endl;
+	my_env[0].append("74");
+	my_env[1].append("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+	my_env[3].append("http://localhost/Users/sel-fadi/Desktop/webserv/cgi/test.php");
+	my_env[4].append("--");
+	my_env[5].append("--");
+	my_env[6].append("GET");
+	my_env[7].append("--");
+	my_env[8].append("/test.php");
+	my_env[9].append("--");
+	my_env[10].append("--");
+	// std::cout << my_env[3] <<  std::endl;
+	return my_env;
 }
 
 int main()
 {
-    std::map<char **, std::string> my_map;
-    int fd[2];
-    pid_t pid;
-    int ret;
-    char **tmp;
-    char **env;
-    tmp = new char*[3];
-    std::string arg = "/Users/sel-fadi/Desktop/webserv/cgi/test.py";
-    std::string scriptType = "/usr/bin/python";
+	int fd[2];
+	pid_t pid;
+	int ret;
+	char **tmp;
+	
+	std::vector<std::string> my_headers = setEnvInVector();
+	char *const*env;
+	tmp = new char*[3];
+	std::string arg = "/Users/sel-fadi/Desktop/webserv/cgi/test.php";
+	std::string scriptType = "/usr/bin/php";
 
-    tmp[0] = strdup(scriptType.c_str());
-    tmp[1] = strdup(arg.c_str());
-    tmp[2] = NULL;
-    
-    // my_map.insert(std::make_pair("REDIRECT_STATUS", "200"));
-    // my_map.insert(std::make_pair("REQUEST_METHOD", "GET"));
-    // my_map.insert(std::make_pair("SCRIPT_FILENAME", arg));
-    // my_map.insert(std::make_pair("SCRIPT_NAME", "/test.py"));
-    
-
-    // ENV
-    for (int i = 0; i < 4; i++)
-    {
-        // std::cout << "<tr><td>" << ENV[i] << "</td><td>" << std::endl;
-        std::cout << ENV[i] << std::endl;
-        std::cout << std::getenv(ENV[i].c_str()) << std::endl;
-        const char *value = std::getenv(ENV[i].c_str());
-        if (value != 0)
-            std::cout << value;
-        else
-            std::cout << "Environment variables does not exist." << std::endl;
-        // std::cout << "</td></tr>\n";
-    }
-    // std::cout << ;
-    // std::cout << ;
-    pid = fork();
-    if (pid == -1)
-        std::cout << "Something wrong in forking" << std::endl;
-    else if (pid == 0)
-    {
-        // I should exec cgi
-        std::cout << "Iam in the child process." << std::endl;
-        ret = execve(tmp[0], tmp, NULL);
-        if (ret == -1)
-            std::cout << "Execve failed." << std::endl;
-        else
-            std::cout << "Execve Success." << std::endl;
-    }
-    else
-    {
-        // waiting the output of the child
-        std::cout << "Iam in the parent process" << std::endl;
-        // close(fd[0]);
-        // close(fd[1]);
-    }
+	tmp[0] = strdup(scriptType.c_str());
+	tmp[1] = strdup(arg.c_str());
+	tmp[2] = NULL;
+	env = setEnv(my_headers);
+	
+	pid = fork();
+	if (pid == -1)
+		std::cout << "Something wrong in forking" << std::endl;
+	else if (pid == 0)
+	{
+		// I should exec cgi
+		std::cout << "Iam in the child process." << std::endl;
+		ret = execve(tmp[0], tmp, env);
+		if (ret == -1)
+			std::cout << "Execve failed." << std::endl;
+		else
+			std::cout << "Execve Success." << std::endl;
+		std::cout << "Ana hna." << std::endl;
+	}
+	else
+	{
+		// waiting the output of the child
+		std::cout << "Iam in the parent process" << std::endl;
+		// close(fd[0]);
+		// close(fd[1]);
+	}
 }
 
 // int main()
