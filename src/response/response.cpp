61f064876c9f0ce73c371e87713438c97ce133df
Ayoub ABOUNAKIF <aabounak@Ayoubs-MacBook-Pro.local>
2022-02-22 13:57:23 +0100

    X

--- src/response/response.cpp
+++ src/response/response.cpp
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   response.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abiari <abiari@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 11:14:05 by abiari            #+#    #+#             */
-/*   Updated: 2022/02/22 13:48:33 by abiari           ###   ########.fr       */
+/*   Updated: 2022/02/22 13:57:05 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "response.hpp"
 
-response::response(){}
-response::~response(){}
+response::response() {}
+response::~response() {}
+
+void response::__getResrc( std::string path ) {
+    
+    return ;
+}
+
+void response::serveRequest( Request req ) {
+    
+    return ;
+}
--- src/response/response.hpp
+++ src/response/response.hpp
@@ -3,35 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   response.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abiari <abiari@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 11:14:05 by abiari            #+#    #+#             */
-/*   Updated: 2022/02/22 13:45:29 by abiari           ###   ########.fr       */
+/*   Updated: 2022/02/22 13:56:01 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEBSERV_RESPONSE_HPP
 #define WEBSERV_RESPONSE_HPP
+
 #include "../config/ConfigFile.hpp"
 #include "../request/Request.hpp"
+
 class response {
 	public:
 		response();
-		response(const response &x);
+		response( const response &x );
 		~response();
-		response&	operator=(const response &x);
+		response&	operator=( const response &x );
 
-		void	getResrc(std::string path);
-		//check if file exists, if yes then check if it's a file or a folder
-		//if file, get its data into a response with headers either raw 
-		//or from cgi output if file is script, else call autoindexmodule
-		void	postResrc(std::string path);
-		void	deleteResrc(std::string path);
-		void	serveRequest(Request req);
-		void	autoindexModule(void);
+	private:
+		void	__getResrc( std::string path );
+		void	__postResrc( std::string path );
+		void	__deleteResrc( std::string path );
+		void	__autoindexModule( void );
+		
+	public:
+		void	serveRequest( Request req );
+		
 	private:
 		std::string	res;
 };
 
-
-#endif //WEBSERV_RESPONSE_HPP
+#endif // WEBSERV_RESPONSE_HPP
