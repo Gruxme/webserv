90a9278ac45eadcd3929b1d817335045a3665e87
Ayoub ABOUNAKIF <aabounak@Ayoubs-MacBook-Pro.local>
2022-02-22 14:22:58 +0100

    XXx

--- src/config/LocationClass.cpp
+++ src/config/LocationClass.cpp
@@ -6,14 +6,14 @@
 /*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 15:45:04 by aabounak          #+#    #+#             */
-/*   Updated: 2022/02/22 14:15:45 by aabounak         ###   ########.fr       */
+/*   Updated: 2022/02/22 14:22:21 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "LocationClass.hpp"
 
 /* ----- Constructors & Destructor respectively ----- */
-LocationClass::LocationClass() : __path(""), __root(""), __cgi_ext("") {}
+LocationClass::LocationClass() : __path(""), __root(""), __cgi_ext(""), __autoindex(__AUTOINDEX_OFF__) {}
 LocationClass::~LocationClass() {}
 
 /* ----- Location Parser ----- */
@@ -22,9 +22,10 @@ void    LocationClass::parseLocation( std::string buffer ) {
         this->__path = buffer.substr(buffer.find("path = ") + strlen("path = "));
     else if (buffer.find("root = ") != std::string::npos)
         this->__root = buffer.substr(buffer.find("root = ") + strlen("root = "));
-    else if (buffer.find("cgi_ext") != std::string::npos) {
+    else if (buffer.find("cgi_ext") != std::string::npos)
         this->__cgi_ext = buffer.substr(buffer.find("cgi_ext = ") + strlen("cgi_ext = "));
-    }
+    else if (buffer.find("autoindex = on") != std::string::npos)
+        this->__autoindex = __AUTOINDEX_ON__;
 }
 
 /* -- PUBLIC METHODS */
@@ -49,3 +50,4 @@ std::vector<std::string> LocationClass::split( std::string str, char separator )
 std::string LocationClass::getPath( void ) const { return this->__path; }
 std::string LocationClass::getRoot( void ) const { return this->__root; }
 std::string LocationClass::getCgiExt( void ) const { return this->__cgi_ext; }
+bool        LocationClass::getAutoIndex( void ) const { return this->__autoindex; }
--- src/config/LocationClass.hpp
+++ src/config/LocationClass.hpp
@@ -6,7 +6,7 @@
 /*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 01:50:10 by aabounak          #+#    #+#             */
-/*   Updated: 2022/02/22 14:15:40 by aabounak         ###   ########.fr       */
+/*   Updated: 2022/02/22 14:21:06 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 # include <string>
 # include <vector>
 
+# define __AUTOINDEX_OFF__ 0
+# define __AUTOINDEX_ON__ 1
+
 class LocationClass {
     /* ----- PRIVATE ----- */
     public:
         std::string __path;
         std::string __root;
         std::string __cgi_ext;
+        bool        __autoindex;
         
     public:
         /* ----- Constructors & Destructor respectively ----- */
@@ -37,4 +41,5 @@ class LocationClass {
         std::string getPath( void ) const;
         std::string getRoot( void ) const;
         std::string getCgiExt( void ) const;
+        bool        getAutoIndex( void ) const;
 };
