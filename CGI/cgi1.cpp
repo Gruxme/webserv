7494ef8a46bb100747dbdc10ecd34ad959010de4
sel-fadi <sel-fadi@student.1337.ma>
2022-03-05 17:17:59 +0100

    CGI in process

--- CGI/cgi1.cpp
+++ CGI/cgi1.cpp
@@ -6,28 +6,28 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 12:17:14 by sel-fadi          #+#    #+#             */
-/*   Updated: 2022/02/24 14:45:05 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2022/03/05 17:17:49 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cgi.hpp"
 
+extern char **environ;
 cgi::cgi()
 {
     this->getOrPost = 1;
-    this->queryString = "fname=hey";
-    // std::string arg = "/Users/sel-fadi/Desktop/webserv/cgi/test.py";
+    // this->queryString = "fname=hey";
+    std::string arg = "/Users/sel-fadi/Desktop/webserv/cgi/test.py";
 	// std::string scriptType = "/usr/bin/python";
-    // this->arg = "/Users/sel-fadi/Desktop/webserv/cgi/test1.php";
-    this->arg = "/Users/sel-fadi/Desktop/webserv/cgi/test.php";
+    this->arg = "/Users/sel-fadi/Desktop/OurWebserv/CGI/test.php";
+    // // this->arg = "/Users/sel-fadi/Desktop/webserv/cgi/test.php";
 	this->scriptType = "/Users/sel-fadi/.brew/bin/php-cgi";
-    this->my_headers = setEnvInVector(request);
-	this->my_headerPost = setEnvInVectorPost(request);
+    // this->my_headers = setEnvInVector(request);
+	// this->my_headerPost = setEnvInVectorPost(request);
 }
 
 cgi::~cgi()
 {
-    
 }
 
 char *const* cgi::setEnv(std::vector<std::string> my_env)
@@ -54,75 +54,89 @@ std::vector<std::string> cgi::setEnvInVectorPost(Request &my_request)
 
 	my_env[0].append("9");
 	my_env[1].append("fname=hey");
-	my_env[3].append("/Users/sel-fadi/Desktop/webserv/cgi/test.php");
+	my_env[3].append("/Users/sel-fadi/Desktop/OurWebserv/CGI/test.php");
 	return my_env;
 }
 
 std::vector<std::string> cgi::setEnvInVector(Request &my_request)
 {
 	std::vector<std::string> my_env;
-	my_env.push_back("CONTENT_LENGTH=");
-	my_env.push_back("CONTENT_TYPE=");
-	my_env.push_back("GATEWAY_INTERFACE=CGI/1.1");
-	my_env.push_back("PATH_INFO=");
-	my_env.push_back("PATH_TRANSLATED=");
-	my_env.push_back("QUERY_STRING=");
-	my_env.push_back("REQUEST_METHOD=");
-	my_env.push_back("REMOTE_ADDR=");
-	my_env.push_back("SCRIPT_NAME=");
-	my_env.push_back("SERVER_NAME=");
-	my_env.push_back("SERVER_PORT=");
+	// my_env.push_back("CONTENT_LENGTH=");
+	// my_env.push_back("GATEWAY_INTERFACE=CGI/1.1");
+	my_env.push_back("PATH_INFO=/test.php");
+	// my_env.push_back("PATH_TRANSLATED=");
+	my_env.push_back("QUERY_STRING=\"var1=value1\"");
+	my_env.push_back("REQUEST_METHOD=GET");
+	// my_env.push_back("REMOTE_ADDR=");
+	my_env.push_back("SCRIPT_NAME=web");
+	my_env.push_back("SERVER_NAME=ff");
+	my_env.push_back("SERVER_PORT=8888");
 	my_env.push_back("SERVER_PROTOCOL=HTTP/1.1");
+	// setenv()
 	my_env.push_back("SERVER_SOFTWARE=webserv");
 	my_env.push_back("REDIRECT_STATUS=200");
+	my_env.push_back("SCRIPT_FILENAME=test.php");
 
 	// my_env[0].append(my_request.getHeaders().find("CONTENT_LENGTH")->second);
 	// std::cout << my_request.getHeaders() << std::endl;
-	my_env[1].append("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
-	my_env[3].append("http://localhost/Users/sel-fadi/Desktop/webserv/cgi/test.php");
-	my_env[4].append("--");
-	my_env[5].append("--");
-	my_env[6].append("GET");
-	my_env[7].append("--");
-	my_env[8].append("/test.php");
-	my_env[9].append("--");
-	my_env[10].append("--");
+	// my_env[1].append("f");
+	// my_env[3].append("/test.php");
+	// my_env[4].append("--");
+	// my_env[5].append("--");
+	// my_env[6].append("GET");
+	// my_env[7].append("--");
+	// my_env[8].append("/test.php");
+	// my_env[9].append("--");
+	// my_env[10].append("--");
+	// my_env[14].append("/Users/sel-fadi/Desktop/OurWebserv/CGI/test.php");
 	return my_env;
 }
-
+     #include <stdlib.h>
 void cgi::exec_script(int *fd, int *fd1)
 {
     int ret;
-    char **tmp;
+    char *tmp[3];
     char *const*env;
-
-    tmp = new char*[3];
-	tmp[0] = strdup(scriptType.c_str());
-	tmp[1] = strdup(arg.c_str());
+    // tmp = new char*[3];
+	tmp[0] = (char*)scriptType.c_str();
+	tmp[1] = (char*)arg.c_str();
 	tmp[2] = NULL;
-    if (this->getOrPost)
-		env = setEnv(this->my_headerPost);
-	else
-		env = setEnv(this->my_headers);
-    if (this->getOrPost)
-    {
-        fd1[1] = open("PostResponse.txt", O_WRONLY | O_CREAT, 0777);
-        dup2(fd1[1], STDOUT_FILENO);
+
+    // if (this->getOrPost)
+	// 	env = setEnv(this->my_headerPost);
+	// else
+	// 	env = setEnv(this->my_headers);
+    // if (this->getOrPost)
+    // {
+		// setenv("CONTENT_LENGTH","9", 1);
+		// setenv("SERVER_PROTOCOL","HTTP/1.1", 1);
+		// setenv("QUERY_STRING","fname=hey", 1);
+		// setenv("REQUEST_METHOD","POST", 1);
+		// setenv("REDIRECT_STATUS","200",1);
+		// setenv("CONTENT_TYPE", "text/html; charset=UTF-8",1);
+		// setenv("SCRIPT_FILENAME", "/Users/sel-fadi/Desktop/OurWebserv/CGI/test.php",1);
+        dup2(fd1[0], 0);
         write(fd1[1], this->queryString, strlen(this->queryString));
-    }
-    else
-    {
-        fd[1] = open("GetResponse.txt", O_WRONLY | O_CREAT, 0777);
-        dup2(fd[1], STDOUT_FILENO);
-    }
+    // }
+    // else
+    // {
+	// 	setenv("SERVER_PROTOCOL","HTTP/1.1", 1);
+	// 	setenv("QUERY_STRING","v=iBy1Clvr_6o", 1);
+	// 	setenv("PATH_INFO","/test.php", 1);
+	// 	setenv("REQUEST_METHOD","get", 1);
+	// 	setenv("REDIRECT_STATUS","200",1);
+	// 	setenv("SCRIPT_FILENAME", "/Users/sel-fadi/Desktop/OurWebserv/CGI/test.php",1);
+	// }
+	dup2(fd[1], STDOUT_FILENO); 
     close(fd[0]);
     close(fd1[0]);
-    ret = execve(tmp[0], tmp, env);
-    // Status = 500
+    close(fd[1]);
+    close(fd1[1]);
+	
+	// std::cerr << tmp[0] << "  " << tmp[1] << "\n";
+    ret = execve(tmp[0], tmp, environ);
     if (ret == -1)
         exit(EXIT_FAILURE);
-    else
-        exit(EXIT_SUCCESS);
 }
 
 void cgi::script_output(int *fd, int *fd1, pid_t pid)
@@ -132,14 +146,11 @@ void cgi::script_output(int *fd, int *fd1, pid_t pid)
     char buffer[4096];
 
     close(fd[1]);
-    fd[0] = open("GetResponse.txt", O_RDONLY);
-    count = read(fd[0], &buffer, sizeof(buffer));
-    sleep(2);
-    kill(pid, SIGKILL);
-    close(fd[0]);
     close(fd1[0]);
     close(fd1[1]);
-    wait(&status);
+    count = read(fd[0], &buffer, 4096);
+	std::cout << buffer << std::endl;
+    close(fd[0]);
     if (!WEXITSTATUS(status))
         exit(EXIT_SUCCESS);
     else
@@ -153,6 +164,13 @@ void cgi::processing_cgi(Request &request)
 	int fd1[2];
     pid_t pid;
 
+	setenv("CONTENT_LENGTH","9", 1);
+	setenv("SERVER_PROTOCOL","HTTP/1.1", 1);
+	setenv("QUERY_STRING","fname=hey", 1);
+	setenv("REQUEST_METHOD","POST", 1);
+	setenv("REDIRECT_STATUS","200",1);
+	setenv("CONTENT_TYPE", "application/x-www-form-urlencoded",1);
+	setenv("SCRIPT_FILENAME", "/Users/sel-fadi/Desktop/OurWebserv/CGI/test.php",1);
 	if (pipe(fd) == -1)
 		exit(EXIT_FAILURE);
 	if (pipe(fd1) == -1)
@@ -165,4 +183,5 @@ void cgi::processing_cgi(Request &request)
 		exec_script(fd, fd1);
     else
         script_output(fd, fd1, pid);
+	wait(NULL);
 }
