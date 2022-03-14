1285cb9189a7fb369cdaac564fa33e9dadade126
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2022-03-14 16:56:20 +0100

    DAKCHI KHDAAAAAME WA TSHBARKELLAH

--- CGI/cgi.cpp
+++ CGI/cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: aabounak <aabounak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 12:17:14 by sel-fadi          #+#    #+#             */
-/*   Updated: 2022/03/14 15:20:49 by aabounak         ###   ########.fr       */
+/*   Updated: 2022/03/14 16:54:56 by aabounak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,10 @@ void cgi::_setEnv()
 	setenv("SERVER_PROTOCOL", _request.getProtocol().c_str(), 1);
 	setenv("REQUEST_METHOD", _request.getMethod().c_str(), 1);
 	setenv("REDIRECT_STATUS","true", 1);
-	setenv("CONTENT_TYPE", _request.getHeaders().find("Content-Type")->second.c_str(), 1);
+	if (_request.getHeaders().find("Content-Type") != _request.getHeaders().end())
+		setenv("CONTENT_TYPE", _request.getHeaders().find("Content-Type")->second.c_str(), 1);
+	else
+		setenv("CONTENT-TYPE", "text/html; charset=utf-8", 1);
 	setenv("SCRIPT_FILENAME", arg.c_str(), 1);
 	setenv("SERVER_PROTOCOL", _request.getProtocol().c_str(), 1);
 	setenv("QUERY_STRING", _request.getQuery().c_str(), 1);
@@ -175,7 +178,8 @@ void	cgi::_parseOutput( int fd ) {
 	std::string tmp = this->_generateTmp(fd);
 	std::stringstream ss(tmp);
 	std::string buffer;
-	std::getline(ss, buffer);
+	std::getline(ss, buffer);;
+
 	if (std::strncmp("Status: ", buffer.c_str(), 8) == 0) {
 		this->_status = buffer.substr(buffer.find("Status: ") + strlen("Status: "));
 		tmp.erase(tmp.begin(), tmp.begin() + buffer.length());
@@ -187,7 +191,6 @@ void	cgi::_parseOutput( int fd ) {
 	_output += date;
 	_output += "\r\nServer: Webserv/4.2.0 \r\n";
 	delete[] date;
-	std::cout << _contentLength << std::endl;
 	_output += "Content-Length: " + std::to_string(_contentLength) + "\r\nConnection: " + _request.getHeaders().find("Connection")->second + "\r\n" + tmp;
 	remove(_tmpOutputFileName.c_str());
 }
